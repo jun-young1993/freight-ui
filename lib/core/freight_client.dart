c65d00d016ab2cbddcd709d77931d52b2c82d58a
kim-jun-young <jykim@gemiso.com>
2023-09-13 16:45:55 +0900

    client_test

--- lib/core/freight_client.dart
+++ lib/core/freight_client.dart
@@ -1,10 +1,20 @@
+// import 'dart:core';
 import 'package:http/http.dart' as http;
 
 class FreightClient {
-  // Uri uri = Uri.https('localhost',);
+  
+  
 
-  // Future get() async {
-  //   http.Response res = 
-  //     await http.get(uri)
-  // }
+  Uri getUri(String unencodedPath, { Map<String, dynamic>? queryParameters } ){
+    return Uri.http('158.180.82.177:3000',unencodedPath, queryParameters);
+  }
+
+
+  Future<http.Response> get(String endpoint) async {
+    http.Response res = 
+      await http.get(getUri(endpoint));
+    return res;
+  }
+  
+  
 }
---
+++ test/client_test.dart
@@ -0,0 +1,19 @@
+import 'dart:developer';
+
+
+import 'package:freight_ui/core/freight_client.dart';
+import 'package:freight_ui/utills/date.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('freight client', (){
+    test('should get drive to request ', () async {
+      // final freightClient = FreightClient();
+      // final response = await freightClient.get('/api/v1/operate');
+      // print(response.statusCode);
+      // print(response.body);
+    });
+    
+
+  });
+}
