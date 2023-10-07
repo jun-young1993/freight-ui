189bf53a45149c69bc1e5d24b7f9379aba9fe059
junyoungkim <juny3738@gmail.com>
2023-10-07 11:54:30 +0900

    delete drive form

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:freight_ui/app.dart';
-import 'package:freight_ui/main_form.dart';
 import 'package:freight_ui/repositories/drive_repository.dart';
 import 'package:freight_ui/repositories/user_repository.dart';
 import 'package:freight_ui/states/drive/drive_bloc.dart';
